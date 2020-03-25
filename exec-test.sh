7144164e0323bf00d95a829e1eece2339482b94c
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-03-25 00:54:09 -0500

    Throws errors as expected

--- exec-test.sh
+++ exec-test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 DIR_NAME=$1
 APP_NAME=$2
@@ -7,16 +8,16 @@ EMAIL=$3
 bash exec-main.sh $DIR_NAME $APP_NAME $EMAIL
 
 num_orig_email=`grep -or 'somebody@rubyonracetracks.com' $PWD/new_apps/$DIR_NAME/* | wc -l`
-if test $num_orig_email -gt 0
+if [ $num_orig_email -gt 0 ];
 then
   echo 'FAIL: at least one instance of somebody@rubyonracetracks.com remains'
   exit 1
 fi
 
 orig_title='Generic App Template'
-num_orig_title=`grep -or 'somebody@rubyonracetracks.com' $PWD/new_apps/$DIR_NAME/* | wc -l`
-if test $num_orig_title -gt 0
+num_orig_title=`grep -or "$orig_title" $PWD/new_apps/$DIR_NAME/* | wc -l`
+if [ $num_orig_title -gt 0 ];
 then
-  echo 'FAIL: at least one instance of somebody@rubyonracetracks.com remains'
+  echo "FAIL: at least one instance of $orig_title remains"
   exit 1
 fi
