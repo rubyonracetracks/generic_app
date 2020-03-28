c0e4b1820b561d67e909bde7487e6170e729fb51
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-03-28 17:00:15 -0500

    Added travis-after.sh

---
+++ travis-after.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e
+
+# This script is used in the Travis environment for testing the new app
+# after it is created.
+
+rvm install `cat $PWD/new_apps/tmp1/.ruby-version`
+cd $PWD/new_apps/tmp1
+
+bundle install --quiet
+rails db:migrate
+
+echo '----------'
+echo 'rails test'
+rails test
+
+echo '----------------------'
+echo 'rails db:migrate:reset'
+rails db:migrate:reset
+
+echo '-------------'
+echo 'rails db:seed'
+rails db:seed
+
+echo '-----------------------------------------'
+echo 'bundle exec annotate -r --models -a -k -i'
+bundle exec annotate -r --models -a -k -i
+
+echo '---------------------------'
+echo 'brakeman -Aq -w2 --no-pager'
+brakeman -Aq -w2 --no-pager
+
+echo '-------------------'
+echo 'bundle-audit update'
+bundle-audit update
+
+echo '------------'
+echo 'bundle-audit'
+bundle-audit
+
+echo '----------'
+echo 'rubocop -D'
+rubocop -D
+
+echo '----------------------'
+echo 'rails_best_practices .'
+rails_best_practices .
+
+echo '----------'
+echo 'gemsurance'
+gemsurance
